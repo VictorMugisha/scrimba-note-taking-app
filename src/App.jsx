1ff165fa25b119c5152fde7a7dba94d45b5b1677
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-07-19 14:49:05 +0200

    Added other feature of moving the most recent note on top of the list

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,24 @@ export default function App() {
     }
     
     function updateNote(text) {
-        setNotes(oldNotes => oldNotes.map(oldNote => {
-            return oldNote.id === currentNoteId
-                ? { ...oldNote, body: text }
-                : oldNote
-        }))
+        setNotes(oldNotes => {
+            const newArray = []
+            for (let oldNote of oldNotes) {
+                if (oldNote.id === currentNoteId) {
+                    newArray.unshift({ ...oldNote, body: text })
+                } else {
+                    newArray.push(oldNote)
+                }
+            }
+            return newArray
+        })
+
+        // Working Code
+        // setNotes(oldNotes => oldNotes.map(oldNote => {
+        //     return oldNote.id === currentNoteId
+        //         ? { ...oldNote, body: text }
+        //         : oldNote
+        // }))
     }
     
     function findCurrentNote() {
